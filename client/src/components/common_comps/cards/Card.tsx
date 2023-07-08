79982c5c0de13d88553ee33955c198d4b54023e8
matterholt <matterholt.dev@gmail.com>
2023-07-08 12:53:07 -0400

    initial card component

---
+++ client/src/components/common_comps/cards/Card.tsx
@@ -0,0 +1,14 @@
+const Cards = ({ dealtCards }: { dealtCards: DealtCards[] }) => {
+  {
+    dealtCards.map((x) => (
+      <div key={x.code}>
+        <img style={{ aspectRatio: "3/4", width: "100px" }} src={x.image} />
+        <h3>
+          {x.suit} <br /> of {x.value}
+        </h3>
+      </div>
+    ));
+  }
+};
+
+export default Cards;
