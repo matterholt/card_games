c723dcf8f247041431d2b1b0025243ffda6f5d54
matterholt <matterholt.dev@gmail.com>
2023-07-31 08:20:18 -0400

    added button

--- client/src/components/common_comps/player_actions/PlayerActions.tsx
+++ client/src/components/common_comps/player_actions/PlayerActions.tsx
@@ -1,13 +1,22 @@
 import actionList from "./playerAction.module.css";
 
+function Button({ children }: { children?: React.ReactNode }) {
+  return (
+    <div>
+      <button>{children}</button>
+    </div>
+  );
+}
+
 function PlayerActions() {
   const tempActionList = ["hold", "draw"];
   return (
     <div>
-      <h2>list of actions to perform on deal</h2>
       <ul className={actionList.ulList}>
         {tempActionList.map((x) => (
-          <li>{x}</li>
+          <li>
+            <Button>{x}</Button>
+          </li>
         ))}
       </ul>
     </div>
